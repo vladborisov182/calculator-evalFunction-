78eb749467a8ecf4c80c91ebef75e84898528313
Vlad Borisov <vladborisov182@gmail.com>
2017-11-15 00:04:47 +0200

    correction

--- python3-calc-v2.py
+++ python3-calc-v2.py
@@ -1,28 +1,36 @@
-#Функция, проверяющая правильность ввода
+#!/usr/bin/python3
+
 def num_test(str):
+    
+    '''
+    Функция, проверяющая правильность ввода
+    '''
+    
     while True:
         try:
             x = float(input("Введите " + str + " значение: "))
             return x
         except ValueError:
-            print("Вы ввели неправильный символ!")
-            continue
-        break
-#Сама программа
-print("Для завершения программы введите q вместо знака операции")
-while True:
-    s = input("Введите знак операции (+, -, *, /)")
-    if s is "q":
-        print("Программа завершена")
-        break
-    if s in ("+", "-", "*", "/"):
-        a = str(num_test("первое"))
-        b = str(num_test("второе"))
-# Логика калькулятора и вывод ответа
-        try:
-            print('{} + {} = '.format(a, b), eval(a + s + b))
-        except ZeroDivisionError:
-            print('{} / {} '.format(a, b))
-            print("Деление на ноль!")
-    else:
-        print("Неверный знак")
+            print("Вы ввели неправильный символ")
+        else:
+            break
+
+
+if __name__ == "__main__":
+    print("Для завершения программы введите q вместо знака операции")
+    while True:
+        s = input("Введите знак операции (+, -, *, /)")
+        if s == "q":
+            print("Программа завершена")
+            break
+        elif s in ("+", "-", "*", "/"):
+            a = str(num_test("первое"))
+            b = str(num_test("второе"))
+    # Логика калькулятора и вывод ответа
+            try:
+                print('{} + {} = '.format(a, b), eval(a + s + b))
+            except ZeroDivisionError:
+                print('{} / {} '.format(a, b))
+                print("Деление на ноль!")
+        else:
+            print("Неверный знак")
